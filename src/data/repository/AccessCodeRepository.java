c83aa1194bb7de330ab0d07714034b588258b8a7
Quayyum-a <quayyumariyo@gmail.com>
2025-05-13 08:14:43 +0100

    Estate Visitor System

--- src/data/repository/AccessCodeRepository.java
+++ src/data/repository/AccessCodeRepository.java
@@ -2,10 +2,12 @@ package data.repository;
 
 import data.model.AccessCode;
 
+import java.time.LocalDateTime;
+
 public interface AccessCodeRepository extends CrudRepository<AccessCode, Integer>{
     AccessCode findByCode(String code);
     AccessCode findByStatus(String status);
     AccessCode findByVisitorId(int visitorId);
     AccessCode findByResidentId(int residentId);
-    AccessCode findByCreatedTime(String createdTime);
+    AccessCode findByCreatedTime(LocalDateTime createdTime);
 }
